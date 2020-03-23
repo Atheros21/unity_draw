d29a4b68dc0db05599909f15ad571c2eaf2c842c
TudureanIulian <iulian.tudurean@assist.ro>
2020-03-23 09:32:46 +0200

    first commit

---
+++ Assets/PainLayerManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ATH
+{
+
+    public class PainLayerManager : MonoBehaviour
+    {
+        [SerializeField] private Vector2 layerCreartionSize;
+        private List<PaintLayer> paintLayers = new List<PaintLayer>();
+        private Canvas parentCanvas;
+
+        void Start()
+        {
+            parentCanvas = GetComponentInParent<Canvas>();
+            CreateLayer(1024, 1024);
+        }
+
+        void Update()
+        {
+
+        }
+
+        private void CreateLayer(int width, int height)
+        {
+            Texture2D texture2D = new Texture2D(height, width);
+            GameObject go = new GameObject("Layer " + paintLayers.Count, typeof(RectTransform), typeof(RawImage));
+            RectTransform rectTransform = go.GetComponent<RectTransform>();
+            rectTransform.anchoredPosition = new Vector2(parentCanvas.pixelRect.width / 2, parentCanvas.pixelRect.height / 2);
+            rectTransform.sizeDelta = new Vector2(layerCreartionSize.x, layerCreartionSize.y);
+            go.transform.SetParent(transform);
+            PaintLayer paintLayer = new PaintLayer(texture2D, go.GetComponent<RawImage>());
+            paintLayer.SetColor(new Color(0, 0, 0, 0));
+            paintLayers.Add(paintLayer);
+        }
+    }
+}
---
+++ Assets/PaintLayer.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace ATH
+{
+    public class PaintLayer
+    {
+        private Texture2D texture;
+        private RawImage rawImage;
+
+        public PaintLayer(Texture2D texture, RawImage rawImage)
+        {
+            this.texture = texture;
+            this.rawImage = rawImage;
+            this.rawImage.texture = this.texture;
+        }
+
+        public void SetColor(Color targetColor)
+        {
+            for (int i = 0; i < texture.width; i++)
+            {
+                for (int j = 0; j < texture.height; j++)
+                {
+                    texture.SetPixel(i, j, targetColor);
+                }
+            }
+            texture.Apply();
+        }
+
+        public void PaintPixel(int i, int j, Color targetColor)
+        {
+            texture.SetPixel(i, j, targetColor);
+        }
+
+        public void IncreasePriority()
+        {
+            int currentOrder = rawImage.transform.GetSiblingIndex();
+            rawImage.transform.SetSiblingIndex(++currentOrder);
+        }
+        public void DecreasePriority()
+        {
+            int currentOrder = rawImage.transform.GetSiblingIndex();
+            rawImage.transform.SetSiblingIndex(currentOrder>1?currentOrder-1:0);
+        }
+    }
+}
